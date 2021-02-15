ea4a2fcb59affb8e437dc07b24c61a860392328c
ejuliette <62560788+ejuliette@users.noreply.github.com>
2021-02-15 16:25:10 +0100

    Récupération des coordonnées fonctionnel !!!!!!!

--- dragdrop/src/app/app.component.ts
+++ dragdrop/src/app/app.component.ts
@@ -22,13 +22,23 @@ export class AppComponent  {
 
 
 
-  create(event){
+  onDragEnded(event) {
+    let element = event.source.getRootElement();
+    let boundingClientRect = element.getBoundingClientRect();
+    let parentPosition = this.getPosition(element);
+    console.log('x: ' + (boundingClientRect.x - parentPosition.left), 'y: ' + (boundingClientRect.y - parentPosition.top));
     
-        const factory = this.resolver.resolveComponentFactory(DraggableComponent);
-        this.componentRef = this.entry.createComponent(factory);
-        this.count++;
-        this.componentRef.instance.text = "Draggble" +this.count;
-        
+  }
+
+  getPosition(el) {
+    let x = 0;
+    let y = 0;
+    while(el && !isNaN(el.offsetLeft) && !isNaN(el.offsetTop)) {
+      x += el.offsetLeft - el.scrollLeft;
+      y += el.offsetTop - el.scrollTop;
+      el = el.offsetParent;
+    }
+    return { top: y, left: x };
   }
 
 
@@ -36,6 +46,4 @@ export class AppComponent  {
 
 
 }
-
-
 
