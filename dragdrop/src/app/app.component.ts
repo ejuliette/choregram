59adbb8b7318b5de76d72a0ba46244dca4c2a67a
ejuliette <62560788+ejuliette@users.noreply.github.com>
2021-02-15 14:34:55 +0100

    Ajouter danseurs ok

--- dragdrop/src/app/app.component.ts
+++ dragdrop/src/app/app.component.ts
@@ -7,27 +7,35 @@ import { DraggableComponent } from './components/dragg.component';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Juliette';
+  numbers: number[] = [];
+  count = 0;
+
       componentRef: any;
-      count=0;
   @ViewChild('ele', { read: ViewContainerRef }) entry: ViewContainerRef;
 
-  constructor(private resolver: ComponentFactoryResolver){
+  constructor(private resolver: ComponentFactoryResolver){}
 
+  add() {
+    this.numbers = [ this.numbers.length, ...this.numbers];
+    this.count = this.numbers.length;
   }
+
+
+
   create(event){
-    console.log(event)
-const factory = this.resolver.resolveComponentFactory(DraggableComponent);
+    
+        const factory = this.resolver.resolveComponentFactory(DraggableComponent);
         this.componentRef = this.entry.createComponent(factory);
         this.count++;
         this.componentRef.instance.text = "Draggble" +this.count;
         
   }
-  dropped(){
-    alert("hi")
-  }
+
 
 
 
 
 }
+
+
+
